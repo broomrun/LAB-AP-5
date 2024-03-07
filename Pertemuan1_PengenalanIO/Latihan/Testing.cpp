6dc096872f6a0ac3facd1dda1a53f464fc5a1737
broomrun <chairunnisaq11@gmail.com>
2024-03-07 20:34:14 +0700

    First Commit

---
+++ Pertemuan1_PengenalanIO/Latihan/Testing.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+    system("cls");
+
+    string nama;
+    cout << "Masukkan nama kamu: ";
+    getline(cin, nama);
+    cout << nama;
+} // or alt shift a
