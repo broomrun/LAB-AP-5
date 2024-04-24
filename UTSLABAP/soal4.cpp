9977b1a0db458990110f4e4a1a6e67e670a8cfe5
broomrun <chairunnisaq11@gmail.com>
2024-04-24 23:06:38 +0700

    Materi

--- UTSLABAP/soal4.cpp
+++ UTSLABAP/soal4.cpp
@@ -1,11 +1,11 @@
 #include <iostream>
 using namespace std;
 int main() {
-    int rows = 4; 
-    int cols = 7; 
+    int baris = 7; 
+    int kolom = 4; 
 
-    for (int i = 1; i <= rows; ++i) {
-        for (int j = 1; j <= cols; ++j) {
+    for (int i = 1; i <= baris; ++i) {
+        for (int j = 1; j <= kolom; ++j) {
             if (j == 1 || j == 2) {
                 cout << "*";
             }
